f9736bc718cfa904e18f0eb6b5dbcc3d01a8e0cf
chika <152640307+chika1605@users.noreply.github.com>
2025-07-08 00:02:30 +0600

    initial dates added

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (id, username, email, password, first_name, last_name, role, created_at, updated_at)
-VALUES (1, 'admin', 'admin@example.com', '$2a$10$zMzDZn5W9o7nL/0EyIsHkudcWi6nBbXqSYe8Q9Ek0OgyPV0Uc29Ce', 'System', 'Admin', 'ADMIN', now(), now());
-
-INSERT INTO users (id, username, email, password, first_name, last_name, role, created_at, updated_at)
-VALUES (2, 'manager', 'manager@example.com', '$2a$10$UOqNs/txKc6xRKn7HSkvH.NHy9UMp2VpMoUOKpGyi99sDCEq1B5lq', 'Team', 'Manager', 'MANAGER', now(), now());
